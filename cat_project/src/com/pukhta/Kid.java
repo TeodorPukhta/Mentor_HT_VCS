ebd6aa6a45ceba910cddf6617eeecf5179a8eed1
TeodorPukhta <teodor.pukhta@gmail.com>
2022-02-19 20:05:38 +0200

    added new classes

---
+++ cat_project/src/com/pukhta/Kid.java
@@ -0,0 +1,15 @@
+package com.pukhta;
+
+public class Kid extends Person {
+    public Kid(String name, String surname, int age) {
+        super(name, surname, age);
+    }
+
+    public void eat(){
+        System.out.println("eat");
+    }
+    @Override
+    void voice() {
+        System.out.println("Hello, my name is " + name);
+    }
+}
--- cat_project/src/com/pukhta/Main.java
+++ cat_project/src/com/pukhta/Main.java
@@ -3,6 +3,9 @@ package com.pukhta;
 public class Main {
 
     public static void main(String[] args) {
+        Person kid = new Kid("Teodor","Pukhta",5);
+        kid.voice();
+        ((Kid) kid).eat();
         System.out.println("Meow");
     }
 }
